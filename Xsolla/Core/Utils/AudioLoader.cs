7157b11b761f452bf1a93d47fc1b2f02212c0789
J. Richard Ward <jay@paladinarcher.com>
2024-12-18 10:31:41 -0700

    adding Xsolla for reference

---
+++ Xsolla/Core/Utils/AudioLoader.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System;
+
+namespace Xsolla.Core
+{
+    public static class AudioLoader
+    {
+        private static readonly Dictionary<string, AudioClip> Clips = new Dictionary<string, AudioClip>();
+
+        public static void LoadAudio(string url, Action<AudioClip> callback)
+        {
+            if (string.IsNullOrEmpty(url))
+                throw new ArgumentNullException(nameof(url));
+
+            if (Clips.ContainsKey(url) && Clips[url] != null)
+            {
+                callback?.Invoke(Clips[url]);
+                return;
+            }
+
+            if (!Clips.ContainsKey(url))
+            {
+                Clips[url] = null;
+
+                WebRequestHelper.Instance.AudioRequest(
+                    url,
+                    clip => Clips[url] = clip,
+                    error => XDebug.LogError(error.errorMessage));
+            }
+
+            CoroutinesExecutor.Run(WaitClip(url, callback));
+        }
+
+        private static IEnumerator WaitClip(string url, Action<AudioClip> callback)
+        {
+            yield return new WaitUntil(() => Clips[url] != null);
+            callback?.Invoke(Clips[url]);
+        }
+    }
+}
